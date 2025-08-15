f6ae0476b927477036de08f40b6b1519f2ef9365
axxms1nq <pukjira234547@gmail.com>
2025-08-15 16:06:59 +0700

    V.2: code ws01-04 but 05 not complete

---
+++ workshop03.py
@@ -0,0 +1,19 @@
+# คำสั่งแสดงผลข้อมูลใดๆ ที่หน้าจอ ใช้ฟังชก์ชัน print()
+# string ข้อความ
+print('Hello')
+print('Hi')
+print('''Hey''')
+print("""Hum""")
+
+# number ตัวเลข
+print(48654)   #integer/int
+print(15.4568) #float
+
+# boolean ตรรกะ มีค่า True หรือ False
+print(True)   
+print(False)
+
+# expression นิพจน์ คือ อะไรก็ตามที่มี process การทำงาน เช่น การคำนวณ การเปรียบเทียบ 
+print(10 + 20)
+print(50 > 90)
+print('Bangna' <= 'Bangpoo')
