37de62ef629da13fcb4580c2d3418459876b01a4
axxms1nq <pukjira234547@gmail.com>
2025-09-04 22:10:52 +0700

    workshop1-22 finish

--- workshop06.py
+++ workshop06.py
@@ -1,2 +1,32 @@
-#Operator ตัวดำเนินการ
-
+#Operator ตัวดำเนินการ คือ เครื่องหมาย
+# 1. Asithemetic Opt. เครื่องหมายคำนวณ
+# + - * / **(exponent) %(mod) //(floor)
+print(10 + 3)
+print(10 - 3)
+print(10 * 3)
+print(10 / 3)  # หารปกติ
+print(10 % 3)  # หารเอาเศษ
+print(10 // 3) # หารเอาส่วน
+print(10 ** 3)
+print('+++++++++++++++++')
+# 2. Comparison Opt. เครื่องหมายเปรียบเทียบ ผลลัพธ์ที่มีคือ True หรือ False
+# == , != , > , < , >= , <=
+print(10 >= 25)  # False
+print(999 > 555) # False
+print('Rayong' >= 'Ranong')
+print('+++++++++++++++++')
+# 3. Logical Opt. เครื่องหมายตรรกะ
+# not , and , or
+# ข้อมูลที่จะใช้กับเครื่องหมายมีแค่ True/False และผลลัพธ์ True/False (boolean)
+print(not True)
+print(not False)
+print('+++++++++++++++++')
+print(True and True)    #True
+print(True and False)   #False
+print(False and True)   #False
+print(False and False)  #False
+print('+++++++++++++++++')
+print(True or True)     #True
+print(True or False)    #True
+print(False or True)    #True
+print(False or False)   #False
---
+++ workshop13.py
@@ -0,0 +1,9 @@
+# -พิสูจน์ตรวจสอบครั้งเดียว จริงทำ เท็จไม่ทำ (if)
+numeber = 20
+if  numeber >= 15 :
+     print ('Hello...')
+     print ('Hi...')
+     print ('Hey...')
+     
+     print ('Bye bey...')
+     print ('Ei ei...')
